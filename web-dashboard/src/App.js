ec4b2fb9e07d30697c36b891f520c49e8f87b72e
TinoGozho <2582402@students.wits.ac.za>
2025-06-26 19:38:03 +0200

    inshallah it works

--- web-dashboard/src/App.js
+++ web-dashboard/src/App.js
@@ -1,14 +1,15 @@
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import LoginPage from './pages/login';
 import MenuPage from './pages/MenuPage';
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<LoginPage />} />
         <Route path="/menu" element={<MenuPage />} />
       </Routes>
     </Router>
   );
 }
-
 export default App;
